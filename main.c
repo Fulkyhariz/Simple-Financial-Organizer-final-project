7616c9fb27444d7974ccebc9edde14eb8696234d
Fulkyhariz <63392639+Fulkyhariz@users.noreply.github.com>
2020-12-19 00:23:34 +0700

    Adding Arrange category
    menambahkan mengurutkan dengan kategori tak dung tak dung

--- main.c
+++ main.c
@@ -3,58 +3,82 @@
 #include <string.h>
 #include <time.h>
 #include <windows.h>
+#include <conio.h>
+
+typedef struct uang{
+    struct tm tm;
+    time_t waktu;
+    int uang;
+    int kategori;
+    char note[50];
+    struct uang *next;
+}LinkedList;
 
 typedef struct money{
+    struct tm tm;
     time_t waktu;
     int uang;
     int kategori;
     char note[50];
     struct money *kiri, *kanan;
-}Data;
+}Tree;
 
-Data* newNode(int item);
-void print(Data* root);
-Data* insert(Data* node, int key);
-void arrangeWaktu(Data* node, struct tm waktu, int key);
-void arrangeUang(Data* node, int key);
-void arrangeKategori(Data* node, int key);
+Tree* newNode(int item);
+void print(Tree* root);
+Tree* insert(Tree* node, int key);
+void arrangeWaktu(Tree* node, struct tm waktu, int key);
+void arrangeKategori(Tree* node, int key);
 int getCategory();
 void menuKategori();
+void getTime(Tree *waktu);
+Tree* passBST(Tree *old);
+Tree* arrangeCat(Tree *node, Tree *old, int key);
 
 int main(){
-    while(1){
+    int i;
     int duit;
-    Data *root = NULL;
-    scanf("%d", &duit);
-    root = insert(root, duit);
-    (root->kategori) = getCategory();
-    printf("\nTambahkan Catatan\n");
-    fflush(stdin);
-    gets(root->note);
-    time(&(root->waktu));
-    print(root);
+    Tree *root = NULL;
+    Tree *arrange = NULL;
+    for(i = 0; i<3; i++){
+        scanf("%d", &duit);
+        root = insert(root, duit);
     }
+    arrange = arrangeCat(arrange, root, root->kategori);
+    print(root);
+    print(arrange);
+    getch();
 }
 
-Data* newNode(int item){ 
-    Data* temp = (Data*)malloc(sizeof(Data)); 
+void getTime(Tree *waktu){
+    time_t t = time(NULL);
+    waktu->tm = *localtime(&t);
+    time(&(waktu->waktu));
+}
+
+Tree* newNode(int item){ 
+    Tree* temp = (Tree*)malloc(sizeof(Tree)); 
     temp->uang = item;
+    (temp->kategori) = getCategory();
+    printf("\nTambahkan Catatan\n");
+    fflush(stdin);
+    gets(temp->note);
+    getTime(temp);
     temp->kiri = temp->kanan = NULL;
     return temp;
 }
 
-void print(Data* root){ 
+void print(Tree* root){ 
     if (root != NULL){ 
         print(root->kiri); 
-        printf("%d \n", root->uang);
-        printf("%d\n", root->kategori);
-        printf("%s", ctime(&(root->waktu)));
-        printf("%s ", root->note);
+        printf("%d ", root->uang);
+        printf("%d ", root->kategori);
+        printf(" %.19s", ctime(&(root->waktu)));
+        printf(" %s\n", root->note);
         print(root->kanan);
     }
 }
 
-Data* insert(Data* node, int key){ 
+Tree* insert(Tree* node, int key){ 
     if (node == NULL) 
         return newNode(key);
     if (key < node->uang) 
@@ -63,14 +87,7 @@ Data* insert(Data* node, int key){
         node->kanan = insert(node->kanan, key);
     return node; 
 }
-void arrangeWaktu(Data* node, struct tm waktu, int key){
-    Data* tmp = NULL;
-    tmp = (Data*)malloc(sizeof(Data));
-    if (key < node->uang) 
-        node->kiri = insert(node->kiri, key);
-    else if (key > node->uang) 
-        node->kanan = insert(node->kanan, key);
-}
+
 int getCategory(){
     int s = 0;
     menuKategori();
@@ -80,6 +97,7 @@ int getCategory(){
     }else{
         printf("Invalid input!!");
         Sleep(3000);
+        fflush(stdin);
         getCategory();
     }
 }
@@ -90,4 +108,24 @@ void menuKategori(){
     printf("3.Hiburan\n");
     printf("4.Kebutuhan Sehari-hari\n");
     printf("5.lainnya\n>> ");
+}
+Tree* passBST(Tree *old){ 
+    Tree* new = (Tree*)malloc(sizeof(Tree)); 
+    new->uang = old->uang;
+    new->kategori = old->kategori;
+    strcpy(new->note, old->note);
+    new->tm = old->tm;
+    new->waktu = old->waktu;
+    new->kiri = new->kanan = NULL;
+    return new;
+}
+
+Tree* arrangeCat(Tree *node, Tree *old, int key){ 
+    if (node == NULL) 
+        return passBST(old);
+    if (key < node->uang) 
+        node->kiri = arrangeCat(node->kiri, old->kiri, old->kiri->kategori); 
+    else if (key > node->uang) 
+        node->kanan = arrangeCat(node->kanan, old->kanan, old->kanan->kategori);
+    return node; 
 }
