47b564f516014e76bf689540a50f3dd494023351
Fulkyhariz <63392639+Fulkyhariz@users.noreply.github.com>
2020-12-18 18:54:03 +0700

    fix category bugs
    sudah dapat menyimpan dan menampilkan data dari binary search tree

--- main.c
+++ main.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <windows.h>
 
 typedef struct money{
-    struct tm waktu;
+    time_t waktu;
     int uang;
     int kategori;
     char note[50];
     struct money *kiri, *kanan;
 }Data;
 
-void getTime(struct tm *waktu);
 Data* newNode(int item);
 void print(Data* root);
 Data* insert(Data* node, int key);
@@ -19,27 +19,26 @@ void arrangeWaktu(Data* node, struct tm waktu, int key);
 void arrangeUang(Data* node, int key);
 void arrangeKategori(Data* node, int key);
 int getCategory();
+void menuKategori();
 
 int main(){
+    while(1){
     int duit;
     Data *root = NULL;
     scanf("%d", &duit);
     root = insert(root, duit);
-    getTime(&(root->waktu));
+    (root->kategori) = getCategory();
+    printf("\nTambahkan Catatan\n");
+    fflush(stdin);
+    gets(root->note);
+    time(&(root->waktu));
     print(root);
-}
-
-void getTime(struct tm *waktu){
-    time_t t = time(NULL);
-    *waktu = *localtime(&t);
+    }
 }
 
 Data* newNode(int item){ 
     Data* temp = (Data*)malloc(sizeof(Data)); 
     temp->uang = item;
-    temp->kategori = getCategory();
-    fflush(stdin);
-    gets(temp->note);
     temp->kiri = temp->kanan = NULL;
     return temp;
 }
@@ -48,7 +47,9 @@ void print(Data* root){
     if (root != NULL){ 
         print(root->kiri); 
         printf("%d \n", root->uang);
-        printf("%d %d %d\n", root->waktu.tm_hour);
+        printf("%d\n", root->kategori);
+        printf("%s", ctime(&(root->waktu)));
+        printf("%s ", root->note);
         print(root->kanan);
     }
 }
@@ -71,20 +72,22 @@ void arrangeWaktu(Data* node, struct tm waktu, int key){
         node->kanan = insert(node->kanan, key);
 }
 int getCategory(){
-    int s;
-    printf("Pilih Kategori Transaksi\n" "1.Makanan & Minuman\n"
-    "2.Transportasi\n" "3.Hiburan\n" "4.Kebutuhan Sehari-hari\n" "5.lainnya\n"
-    ">>");
-    scanf("%d ", &s);
-    switch (s){
-    case 1 || 2 || 3 || 4 || 5:
+    int s = 0;
+    menuKategori();
+    scanf("%d", &s);
+    if (s == 1 || s == 2 || s == 3 || s == 4 || s == 5){
         return s;
-        break;
-    
-    default:
+    }else{
         printf("Invalid input!!");
         Sleep(3000);
         getCategory();
-        break;
     }
+}
+void menuKategori(){
+    printf("\nPilih Kategori Transaksi\n");
+    printf("1.Makanan & Minuman\n");
+    printf("2.Transportasi\n"); 
+    printf("3.Hiburan\n");
+    printf("4.Kebutuhan Sehari-hari\n");
+    printf("5.lainnya\n>> ");
 }
