345749812e2b74a105fe149d57c4617170bf4f4c
Fauzan Syawalino <76045497+Hzeelzax@users.noreply.github.com>
2020-12-19 00:21:33 +0700

    nambahin menu di headernya

--- headerfp.h
+++ headerfp.h
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <windows.h>
 #include <conio.h>
-void help();
+/*void help();
 
 void welcome(){
 	int menu;
+	mainMenu:
 	printf("\n");
 	printf("\t\t\t\t\tWELCOME TO FINANCIAL ORGANIZER PROGRAM\n");
 	printf("\t\t\t\t\t======================================\n");
@@ -17,9 +18,23 @@ void welcome(){
 	scanf("%d", &menu);
 	
 	switch (menu){
+		case 1:
+			start();
+			break;
+		case 2:
+			setting();
+			break;	
 		case 3:
 			help();
 			break;
+		case 4:
+			exit(1);
+		default:
+			printf("\nMohon masukkan input yang benar.");
+			Sleep(3000);
+			system("cls");
+			goto mainMenu;
+			
 	}
 }
 
@@ -37,3 +52,54 @@ void help(){
 	system("cls");
 	welcome();
 }
+
+int start(){
+	char optstart;
+	startmenu:
+	system("cls");
+	printf("\nApakah ini merupakan pertama kali anda menggunakan program ini? (Y/N)\n>> ");
+	scanf("%s", &optstart);
+	
+	switch (optstart){
+		case 'Y':
+			setting();
+			break;
+		case 'N':
+			mulai();
+			break;
+		default:
+			printf("\nMohon masukkan input yang benar.");
+			Sleep(3000);
+			goto startmenu;
+	}	
+}
+
+int mulai(){
+	int optmulai;
+	system("cls");
+	printf("Apa yang ingin ada lakukan pada program ini?\n");
+	printf("1. Tambah Transaksi\n");
+	printf("2. Lihat Riwayat\n");
+	printf("3. Lihat analisis pengeluaran\n");
+	printf("4. Kembali ke menu awal\n>> ");
+	scanf("%d", &optmulai);
+	
+	switch (optmulai){
+		case 1:
+			system("cls");
+			transaksi();
+			break;
+		case 2:
+			system("cls");
+			riwayat();
+			break;
+		case 3:
+			system("cls");
+			analisis();
+			break;
+		case 4:
+			system("cls");
+			welcome();
+			break;
+	}
+}*/
