9dfc85a34ea1c2a7ed790b27bf2889961748900b
Fulkyhariz <63392639+Fulkyhariz@users.noreply.github.com>
2020-12-20 11:42:07 +0700

    menambahkan sort time dan juga sort category

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,46 +6,45 @@
 #include <conio.h>
 
 typedef struct uang{
-    struct tm tm;
     time_t waktu;
     long int uang;
     int kategori;
     char note[50];
-    struct uang *next;
-    struct uang *prev;
-}Linked;
+}Data;
 
-void getTime(Linked *waktu){
+typedef struct linkedlist{
+    Data data;
+    struct linkedlist *next;
+    struct linkedlist *prev;
+}Node;
+
+void getTime(Data *waktu){
     time_t t = time(NULL);
-    waktu->tm = *localtime(&t);
     time(&(waktu->waktu));
 }
 
-void swapAll(Linked *current, Linked *index){
+void swapAll(Node *current, Node *index){
     int temp;
     struct tm tmtemp;
     time_t ttemp;
     char stemp[20];
 
-    temp = current->uang;  
-    current->uang = index->uang;  
-    index->uang = temp;
-    temp = current->kategori;  
-    current->kategori = index->kategori;  
-    index->kategori = temp;  
-    tmtemp = current->tm;  
-    current->tm = index->tm;  
-    index->tm = tmtemp;
-    ttemp = current->waktu;  
-    current->waktu = index->waktu;  
-    index->waktu = ttemp;
-    strcpy(stemp, current->note);
-    strcpy(current->note, index->note);
-    strcpy(index->note, stemp);
+    temp = current->data.uang;  
+    current->data.uang = index->data.uang;  
+    index->data.uang = temp;
+    temp = current->data.kategori;  
+    current->data.kategori = index->data.kategori;  
+    index->data.kategori = temp;  
+    ttemp = current->data.waktu;  
+    current->data.waktu = index->data.waktu;  
+    index->data.waktu = ttemp;
+    strcpy(stemp, current->data.note);
+    strcpy(current->data.note, index->data.note);
+    strcpy(index->data.note, stemp);
 }
 
-void sortList(Linked *head) {  
-    Linked *current = NULL, *index = NULL;  
+void sortUang(Node *head) {  
+    Node *current = NULL, *index = NULL;  
     //Check whether list is empty  
     if(head == NULL) {  
         return;  
@@ -57,7 +55,47 @@ void sortList(Linked *head) {
             //Index will point to node next to current  
             for(index = current->next; index != NULL; index = index->next) {  
                 //If current's data is greater than index's data, swap the data of current and index  
-                if(current->uang > index->uang) {  
+                if(current->data.uang > index->data.uang) {  
+                    swapAll(current, index);
+                }  
+            }  
+        }  
+    }  
+}
+
+void sortKategori(Node *head) {  
+    Node *current = NULL, *index = NULL;  
+    //Check whether list is empty  
+    if(head == NULL) {  
+        return;  
+    }  
+    else {  
+        //Current will point to head  
+        for(current = head; current->next != NULL; current = current->next) {  
+            //Index will point to node next to current  
+            for(index = current->next; index != NULL; index = index->next) {  
+                //If current's data is greater than index's data, swap the data of current and index  
+                if(current->data.kategori > index->data.kategori) {  
+                    swapAll(current, index);
+                }  
+            }  
+        }  
+    }  
+}
+
+void sortTime(Node *head) {  
+    Node *current = NULL, *index = NULL;  
+    //Check whether list is empty  
+    if(head == NULL) {  
+        return;  
+    }  
+    else {  
+        //Current will point to head  
+        for(current = head; current->next != NULL; current = current->next) {  
+            //Index will point to node next to current  
+            for(index = current->next; index != NULL; index = index->next) {  
+                //If current's data is greater than index's data, swap the data of current and index  
+                if(difftime(current->data.waktu, index->data.waktu) > 0) {  
                     swapAll(current, index);
                 }  
             }  
@@ -67,22 +105,22 @@ void sortList(Linked *head) {
 
 /* Given a reference (pointer to pointer) to the head of a list
    and an int, inserts a new node on the front of the list. */
-void push(Linked** head_ref)
+void push(Node** head_ref)
 {
     long int s;
     /* 1. allocate node */
-    Linked* new_node = (Linked*)malloc(sizeof(Linked));
+    Node* new_node = (Node*)malloc(sizeof(Node));
  
     /* 2. put in the data  */
     scanf("%d", &s);
-    new_node->uang  = s;
+    new_node->data.uang  = s;
 
-    getTime(new_node);
+    getTime(&(new_node->data));
 
-    new_node->kategori = 5;
+    new_node->data.kategori = 5;
 
     fflush(stdin);
-    gets(new_node->note);
+    gets(new_node->data.note);
  
     /* 3. Make next of new node as head and previous as NULL */
     new_node->next = (*head_ref);
@@ -97,14 +135,14 @@ void push(Linked** head_ref)
 }
   
 /* Given a reference (pointer to pointer) to the head of a list 
-   and a key, deletes the first occurrence of key in linked list */
-void deleteNode(Linked **head_ref, int key) 
+   and a key, deletes the first occurrence of key in Data list */
+void deleteNode(Node **head_ref, int key) 
 { 
     // Store head node 
-    Linked* temp = *head_ref, *prev; 
+    Node* temp = *head_ref, *prev; 
   
     // If head node itself holds the key to be deleted 
-    if (temp != NULL && temp->uang == key) 
+    if (temp != NULL && temp->data.uang == key) 
     { 
         *head_ref = temp->next;   // Changed head 
         free(temp);               // free old head 
@@ -113,36 +151,36 @@ void deleteNode(Linked **head_ref, int key)
 
     // Search for the key to be deleted, keep track of the 
     // previous node as we need to change 'prev->next' 
-    while (temp != NULL && temp->uang != key) 
+    while (temp != NULL && temp->data.uang != key) 
     { 
         prev = temp; 
         temp = temp->next; 
     } 
   
-    // If key was not present in linked list 
+    // If key was not present in Data list 
     if (temp == NULL) return; 
   
-    // Unlink the node from linked list 
+    // Unlink the node from Data list 
     prev->next = temp->next; 
   
     free(temp);  // Free memory 
 } 
   
-// This function prints contents of linked list starting from  
+// This function prints contents of Data list starting from  
 // the given node 
-void printList(Linked *node) 
+void printList(Node *node) 
 { 
-    Linked* last;
+    Node* last;
     printf("\nTraversal in forward direction \n");
     while (node != NULL) {
-        printf(" %d %d %s %.19s\n", node->uang, node->kategori, node->note, ctime(&(node->waktu)));
+        printf(" %d %d %s %.19s\n", node->data.uang, node->data.kategori, node->data.note, ctime(&(node->data.waktu)));
         last = node;
         node = node->next;
     }
  
     printf("\nTraversal in reverse direction \n");
     while (last != NULL) {
-        printf(" %d %d %s %.19s\n", last->uang, last->kategori, last->note, ctime(&(last->waktu)));
+        printf(" %d %d %s %.19s\n", last->data.uang, last->data.kategori, last->data.note, ctime(&(last->data.waktu)));
         last = last->prev;
     }
 }
@@ -151,7 +189,7 @@ void printList(Linked *node)
 int main() 
 { 
     /* Start with the empty list */
-    Linked* head = NULL;
+    Node* head = NULL;
 
     push(&head); 
     push(&head); 
@@ -159,13 +197,16 @@ int main()
     push(&head);
     
 
-    puts("Created Linked List: "); 
+    puts("Created Data List: "); 
     printList(head); 
     deleteNode(&head, 1); 
-    puts("\nLinked List after Deletion of 1: "); 
+    puts("\nData List after Deletion of 1: "); 
     printList(head);
     printf("\n");
-    sortList(head);
+    sortUang(head);
+    printList(head);
+    printf("\n");
+    sortTime(head);
     printList(head);
 
     return 0; 
