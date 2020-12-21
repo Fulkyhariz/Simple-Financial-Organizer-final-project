9a9243d280e5f841bb72feab0428bab6ac16186a
Fauzan Syawalino <76045497+Hzeelzax@users.noreply.github.com>
2020-12-21 11:35:20 +0700

    nambahin fungsi analisis (belum dipecah ber bulan)

--- header_target.h
+++ header_target.h
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int target;
+
 int settarget(){
-	int target;
-	FILE *fptr;
+	FILE *ftarget;
 	
-	fptr = fopen("settarget.txt", "w");
+	ftarget = fopen("settarget.txt", "w");
 	
-	if(fptr == NULL){
+	if(ftarget == NULL){
 		printf("Error! File text tidak ditemukan.");
 		exit(1);
 	}
@@ -16,8 +17,33 @@ int settarget(){
 	scanf("%d", &target);
 	printf("\nTarget pengeluaran berhasil diperbaharui.\n");
 	
-	fprintf(fptr, "%d", target);
-	fclose(fptr);
+	fprintf(ftarget, "%d", target);
+	fclose(ftarget);
 	
 	return 0;
 }
+/*
+int analisis(){
+	float persentase;
+	int total;
+	FILE *fanalisis;
+	
+	fanalisis = fopen("analisis.txt", "a");
+	
+	if(fanalisis == NULL){
+		printf("Error! File text tidak ditemukan.");
+		exit(1);
+	}
+	persentase = (total/target)*100
+	printf("Besarnya pengeluaran yang telah dikeluarkan adalah sebesar Rp %d,00.\n", total);
+	printf("Dengan target yang telah ditetapkan sebelumnya, persentase pengeluaran yang dihasilkan sebesar %.2f%%", persentase);
+	
+	if(persentase > 100){
+		printf("Besarnya pengeluaran anda sudah melebihi dari target yang telah ditetapkan. Mohon untuk lebih bijak dalam mengelola keuangan anda.");
+	}
+	
+	fprintf(fanalisis, "Persentase pengeluaran: %.2f%%", persentase);
+	fclose(fanalisis);
+	
+	return 0;
+} */ 
